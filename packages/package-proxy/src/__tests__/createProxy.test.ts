adcfbaad7f21b379645ad37094b80e62840aa0a7
sl-julienamblard <julien.amblard@seloger.com>
2021-10-12 15:41:15 +0200

    add UT

--- packages/package-proxy/src/__tests__/createProxy.test.ts
+++ packages/package-proxy/src/__tests__/createProxy.test.ts
@@ -1,6 +1,9 @@
 /** @format */
 
-const mockWriteFile = jest.fn()
+const mockConsole = jest.fn()
+global.console.log = mockConsole
+
+const mockWriteFile = jest.fn().mockImplementation((_a, _b, c) => c())
 jest.mock("../utils/writeFile", () => ({
   __esModule: true,
   writeFile: mockWriteFile,
@@ -12,7 +15,7 @@ jest.mock("../utils/findFiles", () => ({
   findFiles: mockFindFile,
 }))
 
-const mockWriteTemplate = jest.fn().mockReturnValue("foo")
+const mockWriteTemplate = jest.fn().mockReturnValue("content data")
 jest.mock("../templates/writeTemplate", () => ({
   __esModule: true,
   writeTemplate: mockWriteTemplate,
@@ -21,7 +24,7 @@ jest.mock("../templates/writeTemplate", () => ({
 import { createProxy } from "../createProxy"
 
 describe("createProxy", () => {
-  test("should call mockWriteFile", () => {
+  test("should call mockWriteFile once", () => {
     mockFindFile.mockReturnValue(["foo"])
     createProxy({
       packageName: "packageName",
@@ -34,5 +37,57 @@ describe("createProxy", () => {
       types: "types",
     })
     expect(mockWriteFile).toHaveBeenCalledTimes(1)
+    expect(mockConsole).toHaveBeenCalledTimes(1)
+    expect(mockWriteFile.mock.calls[0][0]).toEqual("content data")
+    expect(mockWriteFile.mock.calls[0][1]).toEqual("./foo")
+  })
+  test("should call mockWriteFile twice", () => {
+    mockWriteFile.mockReset()
+    mockFindFile.mockReturnValue(["foo", "bar"])
+    createProxy({
+      packageName: "packageName",
+      cjs: "lib",
+      dest: ".",
+      esm: "esm",
+      proxyType: "folder",
+      root: "",
+      src: "src",
+      types: "types",
+    })
+    expect(mockWriteFile).toHaveBeenCalledTimes(2)
+  })
+  test("should call mockWriteFile twice", () => {
+    mockWriteFile.mockReset()
+    mockFindFile.mockReturnValue(["foo", "index.js"])
+    createProxy({
+      packageName: "packageName",
+      cjs: "lib",
+      dest: ".",
+      esm: "esm",
+      proxyType: "folder",
+      root: "",
+      src: "src",
+      types: "types",
+    })
+    expect(mockWriteFile).toHaveBeenCalledTimes(1)
+    expect(mockWriteFile.mock.calls[0][0]).toEqual("content data")
+    expect(mockWriteFile.mock.calls[0][1]).toEqual("./foo")
+  })
+  test("should call mockWriteFile twice", () => {
+    mockWriteFile.mockReset()
+    mockFindFile.mockReturnValue(["foo", "foo.json"])
+    createProxy({
+      packageName: "packageName",
+      cjs: "lib",
+      dest: ".",
+      esm: "esm",
+      proxyType: "folder",
+      root: "",
+      src: "src",
+      types: "types",
+    })
+    expect(mockWriteFile).toHaveBeenCalledTimes(1)
+    expect(mockWriteFile.mock.calls[0][0]).toEqual("content data")
+    expect(mockWriteFile.mock.calls[0][1]).toEqual("./foo")
   })
 })
